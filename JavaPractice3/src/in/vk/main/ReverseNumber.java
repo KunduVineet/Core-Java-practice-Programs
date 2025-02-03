3468a178ba7c4e5090a34b667c924a1d563941ce
VineetKundu <kunduvineet6@gmail.com>
2025-02-03 13:40:34 +0530

    Reverse Number

---
+++ JavaPractice3/src/in/vk/main/ReverseNumber.java
@@ -0,0 +1,28 @@
+package in.vk.main;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the number which you want to Reverse: ");
+		int n = sc.nextInt();
+		
+		int reversed = 0;
+		
+		while(n != 0) {
+			int digits = n %10;
+			reversed = reversed * 10 + digits;
+			n /= 10;
+			
+		}
+		
+		System.out.println("Reversed Number :"+ reversed);
+		sc.close();
+		
+		
+	}
+
+}
