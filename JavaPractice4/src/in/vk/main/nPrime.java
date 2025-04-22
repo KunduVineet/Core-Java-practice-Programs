b0c25a5e511197d135ce15072147d777d5128fa8
VineetKundu <kunduvineet6@gmail.com>
2025-04-22 09:35:58 +0530

    java Practice day 5

--- JavaPractice4/src/in/vk/main/nPrime.java
+++ JavaPractice4/src/in/vk/main/nPrime.java
@@ -1,5 +1,17 @@
 package in.vk.main;
 
 public class nPrime {
+	
+	public static boolean isPrime(int n, int divisor) {
+		if(n <= 1) return false;
+		if(divisor == 1) return true;
+		if(n % divisor == 0) return false;
+		
+		return isPrime(n, divisor-1);
+	}
+	
+	public static void printPrimes(int current, int n) {
+		
+	}
 
 }
