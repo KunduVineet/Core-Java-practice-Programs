0afa13878d027170dfffe67beec6f84781c7bfae
VineetKundu <kunduvineet6@gmail.com>
2025-04-22 10:07:42 +0530

    HashMap

---
+++ HashMap/src/in/vk/main/HashMap1.java
@@ -0,0 +1,19 @@
+package in.vk.main;
+
+import java.util.HashMap;
+
+public class HashMap1 {
+	
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		@SuppressWarnings("rawtypes")
+		HashMap hm = new HashMap();
+		
+		hm.put(101, "Deepak");
+		hm.put(102, "Deepak");
+		hm.put(103, "Deepak");
+		
+		System.out.println(hm);
+	}
+
+}
