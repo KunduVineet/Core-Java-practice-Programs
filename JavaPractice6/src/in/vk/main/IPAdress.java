1b2662271675b55cd0b3f82afafd38b41b571138
VineetKundu <kunduvineet6@gmail.com>
2025-04-29 20:24:13 +0530

    IP Address

---
+++ JavaPractice6/src/in/vk/main/IPAdress.java
@@ -0,0 +1,12 @@
+package in.vk.main;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class IPAdress {
+	public static void main(String[] args) throws UnknownHostException {
+		System.out.println(InetAddress.getLocalHost());
+		
+	}
+
+}
