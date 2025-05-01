ff125ef79aee899cd8480ee23f979abd2c9036fc
VineetKundu <kunduvineet6@gmail.com>
2025-05-01 20:45:42 +0530

    Java 8 - Lambda Expression - Return String Length

--- Functional Interface/src/in/vk/main/LambdaExpression.java
+++ Functional Interface/src/in/vk/main/LambdaExpression.java
@@ -8,6 +8,10 @@ interface i2{
 	public void add(int a, int b);
 }
 
+interface length{
+	public int getLength(String s);
+}
+
 public class LambdaExpression {
 	public static void main(String[] args) {
 		interf i = () -> System.out.println("Lambda Expression Implementation");
@@ -15,5 +19,10 @@ public class LambdaExpression {
 		i2 x = (a,b) -> System.out.println("The Sum is "+(a+b));
 		x.add(34, 5);
 		x.add(90, 0);
+		
+		length o = s -> s.length();
+		System.out.println(o.getLength("Hello"));
+		System.out.println(o.getLength("Invoking Lambda expression"));
+
 	}
 }
