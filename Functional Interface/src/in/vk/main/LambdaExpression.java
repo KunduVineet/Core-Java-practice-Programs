a9e5be3b30cd705841527ac5adab317fe9b96cd0
VineetKundu <kunduvineet6@gmail.com>
2025-05-01 20:40:12 +0530

    Java 8 - Lambda Expression - add 2 numbers program

--- Functional Interface/src/in/vk/main/LambdaExpression.java
+++ Functional Interface/src/in/vk/main/LambdaExpression.java
@@ -4,9 +4,16 @@ interface interf {
 	public void m1();
 }
 
+interface i2{
+	public void add(int a, int b);
+}
+
 public class LambdaExpression {
 	public static void main(String[] args) {
 		interf i = () -> System.out.println("Lambda Expression Implementation");
 		i.m1();
+		i2 x = (a,b) -> System.out.println("The Sum is "+(a+b));
+		x.add(34, 5);
+		x.add(90, 0);
 	}
 }
