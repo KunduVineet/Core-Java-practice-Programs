60c69391a645b472607b2bf3b743365e019b07db
VineetKundu <kunduvineet6@gmail.com>
2025-05-03 22:38:09 +0530

    OOPS - Polymorphism - Method Overriding

---
+++ OOPS/src/in/vk/main/Polymorphism_Overriding.java
@@ -0,0 +1,23 @@
+package in.vk.main;
+
+public class Polymorphism_Overriding {
+
+	public void sleep() {
+		System.out.println("You should sleep now");
+	}
+}
+
+class a extends Polymorphism_Overriding{
+	
+	@Override
+	public void sleep() {
+		System.out.println("Im not sleeping");
+	}
+	
+	public static void main(String[] args) {
+		Polymorphism_Overriding p = new Polymorphism_Overriding();
+		
+		p.sleep();
+		
+	}
+}
