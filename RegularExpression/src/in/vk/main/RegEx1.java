89b37293d47abe6ebcec3966afedd2680f288c2c
VineetKundu <kunduvineet6@gmail.com>
2025-06-04 20:53:09 +0530

    Regular Expression - Example

--- RegularExpression/src/in/vk/main/RegEx1.java
+++ RegularExpression/src/in/vk/main/RegEx1.java
@@ -1,5 +1,18 @@
 package in.vk.main;
 
-public class RegEx1 {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class RegEx1 {
+	
+	public static void main(String[] args) {
+		String regex = "ab*ab";
+		Pattern pattern = Pattern.compile(regex);
+		
+		Matcher matcher = pattern.matcher("aaaaaaab");
+		boolean matches = matcher.matches();
+		
+		System.out.println("result : "+ matches);
+	}
+	
 }
