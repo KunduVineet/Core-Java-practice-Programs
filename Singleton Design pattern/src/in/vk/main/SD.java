20821ee5bcfdb0e20c64caf1e8d7e21c229632b4
VineetKundu <kunduvineet6@gmail.com>
2025-06-06 15:19:56 +0530

    Singleton Design pattern

---
+++ Singleton Design pattern/src/in/vk/main/SD.java
@@ -0,0 +1,16 @@
+package in.vk.main;
+
+public class SD {
+	
+	public static void main(String[] args) {
+		SD sd1 = new SD();
+		
+		
+	}
+
+	private SD() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
