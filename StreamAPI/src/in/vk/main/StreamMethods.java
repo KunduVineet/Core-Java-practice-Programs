d6a30f25e14fbd2ad4bf94003cabd34fa2fa62f0
VineetKundu <kunduvineet6@gmail.com>
2025-06-13 14:02:18 +0530

    Stream Methods

---
+++ StreamAPI/src/in/vk/main/StreamMethods.java
@@ -0,0 +1,17 @@
+package in.vk.main;
+
+import java.util.List;
+
+public class StreamMethods {
+
+	public static void main(String[] args) {
+		
+		List<String> names = List.of("Vineet", "Mayank", "Shivam", "Rajan");
+		
+		names.stream().filter(e->e.startsWith("V")).forEach(e->System.out.println(e));
+		
+		List<Integer> numbers = List.of(1,2,3,4,11,34,56,46,6,7,8,9,10);
+		numbers.stream().map(e->e*e).sorted().forEach(e->System.out.println(e));
+
+	}
+}
