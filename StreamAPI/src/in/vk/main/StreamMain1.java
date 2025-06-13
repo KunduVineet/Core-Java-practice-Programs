fbb4484517a0dd78bc075bccd658e1a60335f98e
VineetKundu <kunduvineet6@gmail.com>
2025-06-13 13:12:14 +0530

    Stream API

--- StreamAPI/src/in/vk/main/StreamMain1.java
+++ StreamAPI/src/in/vk/main/StreamMain1.java
@@ -2,13 +2,15 @@ package in.vk.main;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamMain1 {
 
 	public static void main(String[] args) {
 		
 		//create a list and filter all even numbers from list
-		List<Integer> list1 =  List.of(2,3,4,5,6,7,8);
+		List<Integer> list1 =  List.of(2,3,4,5,6,7,8,9,0,90,98);
 //		list1.add(45);
 		System.out.println(list1);
 		
@@ -22,5 +24,18 @@ public class StreamMain1 {
 		}
 		
 		System.out.println(listeven);
+		
+		//using Stream
+		
+		Stream<Integer> stream = list1.stream();
+		List<Integer> newList =  stream.filter(i -> i%2==0).collect(Collectors.toList());
+		
+		System.out.println(newList);
+		
+		//using stream - compact way to write
+		
+		List<Integer> newList2 = list1.stream().filter(i->i%2==0).collect(Collectors.toList());
+	
+		System.out.println(newList2);
 	}
 }
