4f6d57de6a6e4c1d45988e6b0cdb41e06ab3aabd
VineetKundu <kunduvineet6@gmail.com>
2025-06-13 18:31:42 +0530

    Generics

---
+++ Generics/src/in/vk/main/Generics1.java
@@ -0,0 +1,17 @@
+package in.vk.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generics1 {
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<>();
+		list.add("asdf");
+		list.add("ghjk");
+		list.add("qwer");
+		list.add("tyui");
+		System.out.println(list);
+	}
+
+}
