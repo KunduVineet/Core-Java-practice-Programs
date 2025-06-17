ca6d92944ea77ec6deaa77dbf6dd267f00ea1947
VineetKundu <kunduvineet6@gmail.com>
2025-06-17 19:28:27 +0530

    Right Triangle Pattern

---
+++ Star_Patterns/src/in/vk/main/Right_Triangle.java
@@ -0,0 +1,13 @@
+package in.vk.main;
+
+public class Right_Triangle {
+	public static void main(String[] args) {
+		for(int i =1; i<16; i++) {
+			for(int j = 1; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
