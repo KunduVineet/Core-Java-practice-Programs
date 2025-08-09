3ace2be42e0660e40fe36087d00be9bd01ba1e10
VineetKundu <kunduvineet6@gmail.com>
2025-08-09 18:25:57 +0530

    Consumer

---
+++ Consumer/src/in/vk/main/Consumer1.java
@@ -0,0 +1,14 @@
+package in.vk.main;
+
+import java.util.function.Consumer;
+
+public class Consumer1 {
+	
+	public static void main(String[] args) {
+		Consumer<String> c = s-> System.out.println(s);
+		
+		c.accept("Hello");
+		c.accept("Vineet");
+	}
+
+}
