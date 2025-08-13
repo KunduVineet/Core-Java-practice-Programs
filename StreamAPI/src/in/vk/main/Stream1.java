3657f005165b2533cd8848ed2101927af3a59798
VineetKundu <kunduvineet6@gmail.com>
2025-08-13 13:34:44 +0530

    Streams Examples

---
+++ StreamAPI/src/in/vk/main/Stream1.java
@@ -0,0 +1,18 @@
+package in.vk.main;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Stream1 {
+	
+	public static void main(String[] args) {
+		List<Integer> l = List.of(2,5,67,89,0,12,45,67,89,99,8,67,54,76);
+		System.out.println(l);
+		
+		List<Integer> l2 = l.stream().filter(n -> n% 2 == 0).collect(Collectors.toList());
+		System.out.println(l2);
+	}
+	
+		
+		
+}
