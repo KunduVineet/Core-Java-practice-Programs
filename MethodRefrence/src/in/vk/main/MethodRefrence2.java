8121f8f43cf7f8a907ec4faff94ed037862d42bb
VineetKundu <kunduvineet6@gmail.com>
2025-08-13 13:16:22 +0530

    Method refrence for Instance methods using :: operator

--- MethodRefrence/src/in/vk/main/MethodRefrence2.java
+++ MethodRefrence/src/in/vk/main/MethodRefrence2.java
@@ -1,12 +1,25 @@
 package in.vk.main;
 
 public class MethodRefrence2 {
+	
+	public void m1() {
+		for(int i = 0; i<10; i++) {
+			System.out.println("Child Thread");
+		}
+	}
 	public static void main(String[] args) {
-		Runnable r = () ->{
-			for(int i = 0; i<10; i++) {
-				System.out.println("Child Thread");
-			}
-		};
+		//Lambda Expression
+//		Runnable r = () ->{
+//			for(int i = 0; i<10; i++) {
+//				System.out.println("Child Thread");
+//			}
+//		};
+		
+		
+		
+		
+		MethodRefrence2 mr2 = new MethodRefrence2();
+		Runnable r = mr2::m1;
 		
 		Thread m2 = new Thread(r);
 		m2.start();
