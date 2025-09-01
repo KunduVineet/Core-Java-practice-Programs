06c411329990ba5dc9bb6d1befa408e0a1481836
VineetKundu <kunduvineet6@gmail.com>
2025-09-01 19:27:20 +0530

    var Keyword - Java 10

---
+++ var keyword/src/in/vk/main/var1.java
@@ -0,0 +1,16 @@
+package in.vk.main;
+
+public class var1 {
+    public static void main(String[] args) {
+        int i = 10;
+        String s = "new";
+
+        var j = 90;
+        var k = 09.07f;
+        var l = true;
+        var m = "String";
+
+        // Corrected print statement
+        System.out.println("i: " + i + ", j: " + j + ", k: " + k + ", l: " + l + ", m: " + m + ", s: " + s);
+    }
+}
