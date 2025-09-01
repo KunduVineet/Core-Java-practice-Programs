5f60445df29cf3b4fd65acea44664e2182f2a166
VineetKundu <kunduvineet6@gmail.com>
2025-09-01 18:56:17 +0530

    takeWhile - Java 9 Stream API Enhancements

---
+++ Stream API Enhancements/src/in/vk/main/takeWhile.java
@@ -0,0 +1,18 @@
+package in.vk.main;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class takeWhile {
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(2,3,4,5,6,7,8,9,10);
+		
+		List<Integer> result = numbers.stream().filter(n -> n% 2 == 0).collect(Collectors.toList());
+		System.out.println(result);
+		
+		List<Integer> reslt = numbers.stream().takeWhile(n -> n% 2 == 0).collect(Collectors.toList());
+		System.out.println(reslt);
+	}
+
+}
